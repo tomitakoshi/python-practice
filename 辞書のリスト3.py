ffc1fe60c47568b59ff8eef53a2616f9e70e7202
tomitakoshi <tomitakoshirosk@gmail.com>
2026-02-09 14:51:52 +0900

    feat: whileループによるタスク連続入力と終了判定の実装

--- 辞書のリスト3.py
+++ 辞書のリスト3.py
@@ -1,17 +1,54 @@
+def check_limit(data_list):
+    total = sum(item["time"] for item in data_list)
+    return total
 
-todo_list = []
+def main():
+    
+    todo_list = []
 
-"""
-todo_list.append({"name": "掃除", "time": 1})
-todo_list.append({"name": "Python学習", "time": 2})
-todo_list.append({"name":"料理","time":1})
-"""
+    """
+    todo_list.append({"name": "掃除", "time": 1})
+    todo_list.append({"name": "Python学習", "time": 2})
+    todo_list.append({"name":"料理","time":1})
+    """
+    # 初期値入力
+    todo_list.extend([
+        {"name": "掃除", "time": 1},
+        {"name": "Python学習", "time": 2},
+        {"name":"料理","time":1}
+    ])
 
-todo_list.extend([
-    {"name": "掃除", "time": 1},
-    {"name": "Python学習", "time": 2},
-    {"name":"料理","time":1}
-])
 
-print(todo_list)
+    # ユーザー入力
+    print("--- タスク登録 （'None'と入力で終了）---")
+    while True:
+        new_task_name = input("タスク名を入力してください: ")
+
+        if new_task_name == "None":
+            break
+        
+        # 時間の入力
+        new_task_time = int(input(f"「{new_task_name}」の時間を入力してください: "))
+        
+        # リストに追加
+        todo_list.append({"name": new_task_name, "time": new_task_time})
+    
+    """
+    new_task_name = "Javascript勉強"
+    new_task_time = 3
+    todo_list.append({"name": new_task_name, "time": new_task_time})
+    """
+
+    total = check_limit(todo_list)
+    print(f"現在の合計時間は {total} 時間です。")
+    # ルールとの照合（1日は24時間 - 睡眠7時間 = 活動可能17時間）
+    limit_hours = 17 
+    if total > limit_hours:
+        print("⚠️ 警告：活動時間が17時間を超えています！7時間の睡眠が確保できません！")
+    else:
+        print("✅ 順調です。しっかり睡眠も確保できそうですね。")
+
+
+if __name__ == "__main__":
+    main()
 
