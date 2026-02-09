827bd446bdee6269949a636d35ffeaba8423fd8d
tomitakoshi <tomitakoshirosk@gmail.com>
2026-02-09 14:27:18 +0900

    extend学習

--- 辞書のリスト3.py
+++ 辞書のリスト3.py
@@ -1,10 +1,17 @@
 
 todo_list = []
 
-# appendで追加
+"""
 todo_list.append({"name": "掃除", "time": 1})
 todo_list.append({"name": "Python学習", "time": 2})
 todo_list.append({"name":"料理","time":1})
+"""
+
+todo_list.extend([
+    {"name": "掃除", "time": 1},
+    {"name": "Python学習", "time": 2},
+    {"name":"料理","time":1}
+])
 
 print(todo_list)
 
