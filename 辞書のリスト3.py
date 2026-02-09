910823988c1f19d975479779f83e1f76b2436a80
tomitakoshi <tomitakoshirosk@gmail.com>
2026-02-09 14:22:03 +0900

    append学習

---
+++ 辞書のリスト3.py
@@ -0,0 +1,10 @@
+
+todo_list = []
+
+# appendで追加
+todo_list.append({"name": "掃除", "time": 1})
+todo_list.append({"name": "Python学習", "time": 2})
+todo_list.append({"name":"料理","time":1})
+
+print(todo_list)
+
